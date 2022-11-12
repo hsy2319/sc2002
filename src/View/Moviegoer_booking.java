5c974daaadba4ab64c1c28184acb31b3f8422ec8
realannieqiu <116433472+realannieqiu@users.noreply.github.com>
2022-11-13 06:52:23 +0800

    Update Moviegoer_booking.java

--- src/View/Moviegoer_booking.java
+++ src/View/Moviegoer_booking.java
@@ -1,5 +1,5 @@
 // show now showing movies, show showtimes, allow moviegoer to select showtime, seat and book
-package View;
+/*package View;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -32,3 +32,73 @@ public class Moviegoer_booking extends View{
 		 */
 	}
 }
+*/
+//UPDATED BY QIUZHEN
+import java.util.Scanner;
+
+public class MovieGoer_Booking extends View {
+		//Variables
+			private Seat seat;
+			private double basePrice;
+			private String ticketType;
+			private boolean isBooked;
+
+
+		//Constructor
+			public MovieGoer_Booking(Seat seat){
+				this.seat=seat;
+				basePrice= //TO ADD seat.showtime
+				
+				
+						
+			
+		}
+		//Methods
+			
+			public void displayMenu() {
+				
+				
+			}
+			
+			
+			private void getBasePrice() {
+				//Check if holiday or weekend
+				
+			}
+			
+			private void printBookingDetail() {
+				//print name of movie, date and time, location, seat
+				//rating of movie
+				
+				
+			}
+			
+			private void promptCustomerInformation() {
+				Scanner sc = new Scanner(System.in);
+				String name;
+				String email;
+				String mobile;
+				boolean isSenior;
+				String isSenAns;
+				
+		        System.out.println("Please enter your name:");
+		        name=sc.toString();
+		        System.out.println("Please enter your mobile number:");
+		        mobile=sc.toString();
+		        System.out.println("Please enter your Email address:");
+		        email=sc.toString();
+		        System.out.println("Enter Yes if you are a senior citizen or a student?");
+		        System.out.println("Please enter YES if applies and NO otherwise");
+		        isSenAns=sc.toString();
+		        if(isSenAns=="YES") isSenior=true;
+		
+		        // Create moviegoer_data object
+		        MovieGoer_data cust = new MovieGoer_data(name, mobile, email, isSenior);
+
+		        // proceed to payment how
+
+		    }
+
+
+}
+
